888c449059bce16869a79a47c189c123e52961be
Fábio Santos <fabiotrabmat@gmail.com>
2024-10-15 10:06:13 -0300

    feat: adicionando o tratamento do erro.

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ $(function () {
 
 		fetch(endpoint)
 			.then((res) => {
+				if (!res.ok) throw new Error("Usuário inválido");
 				return res.json();
 			})
 			.then((data) => {
+				console.log(data);
 				$("#avatar").attr("src", data.avatar_url);
 				$("#name").text(data.name);
 				$("#user-name").text(data.login);
 				$("#repo-number").text(data.public_repos);
 				$("#followers-number").text(data.followers);
 				$("#following-number").text(data.following);
+			})
+			.catch((error) => {
+				alert(`O nome do usuário não foi encontrado. Verifique o código\n${error}`);
 			});
 	});
 });
